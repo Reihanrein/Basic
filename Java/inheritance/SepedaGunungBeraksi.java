9963842a67455a8802e4ceb043e66e5d04900b60
Reihanrein <99407208+Reihanrein@users.noreply.github.com>
2023-07-15 15:47:05 +0700

    Create SepedaGunungBeraksi.java

---
+++ Java/inheritance/SepedaGunungBeraksi.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Inheritance;
+
+/**
+ *
+ * @author ACER
+ */
+public class SepedaGunungBeraksi {
+    public static void main(String[] args) {
+     // Membuat Object
+     SepedaGunung sepedaku = new SepedaGunung();
+     // Memanggil Method Object
+     sepedaku.TambahKecepatan(10);
+     sepedaku.UbahGir(2);
+     sepedaku.setSadel(20);
+    }
+}
