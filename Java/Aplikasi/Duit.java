10343722fec5242b863917ed76908d4746e7de4f
Reihanrein <99407208+Reihanrein@users.noreply.github.com>
2023-08-14 13:59:33 +0700

    Uploading our small project
    to put it simply, this is a simple application that we made using Apache NetBeans.
    This application is intended for cooperative management so that they can make loan calculations. This app is static, uses no database and no login authentication.

---
+++ Java/Aplikasi/Duit.java
@@ -0,0 +1,99 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Aplikasi;
+
+/**
+ *
+ * @author ACER
+ */ 
+public class Duit {
+    public String nl, al, pkj, tgl, np, ang;
+    public String jml, bln, str, sal, uy, sa12;
+
+    public String getNl() {
+        return nl;
+    }
+
+    public void setNl(String val) {
+        nl = val;
+    }
+
+    public String getAl() {
+        return al;
+    }
+
+    public void setAl(String val) {
+        al = val;
+    }
+
+    public String getPkj() {
+        return pkj;
+    }
+
+    public void setPkj(String val) {
+        pkj = val;
+    }
+
+    public String getTgl() {
+        return tgl;
+    }
+
+    public void setTgl(String val) {
+        tgl = val;
+    }
+
+    public String getNp() {
+        return np;
+    }
+
+    public void setNp(String val) {
+        np = val;
+    }
+
+    public String getAng() {
+        return ang;
+    }
+
+    public void setAng(String val) {
+        ang = val;
+    }
+
+    public String getJml() {
+        return jml;
+    }
+
+    public String getBln() {
+        return bln;
+    }
+
+    public void setBln(String val) {
+        bln = val;
+    }
+
+    public String getStr() {
+        return str;
+    }
+
+    public String getSal() {
+        return sal;
+    }
+
+    public void setSal(String val) {
+        sal = val;
+    }
+
+    public String getUy() {
+        return uy;
+    }
+
+    public void setUy(String val) {
+        uy = val;
+    }
+
+    public String getSa12() {
+        return sa12;
+    }
+    
+}
